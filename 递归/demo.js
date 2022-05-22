8bf8d042e7fdad8376a87235d32362ca0578912a
CentChuan <609833577@qq.com>
2022-05-22 20:55:41 +0800

    递归

---
+++ 递归/demo.js
@@ -0,0 +1,69 @@
+let arr = [
+    { id: 1, name: "部门1", pid: 0 },
+    { id: 2, name: "部门2", pid: 1 },
+    { id: 3, name: "部门3", pid: 1 },
+    { id: 4, name: "部门4", pid: 3 },
+    { id: 5, name: "部门5", pid: 4 },
+  ];
+  
+  /**
+   * 递归查找，获取children
+   */
+  const getChildren = (data, result, pid) => {
+    for (const item of data) {
+      if (item.pid === pid) {
+        const newItem = { ...item, children: [] };
+        // newItem={ id: 1, name: "部门1", pid: 0 ,children:[]},  1
+        // newItem={ id: 2, name: "部门2", pid: 1 .children:[]}, 2
+        result.push(newItem);
+        // result=newItem.children
+        //  item.id=1
+        getChildren(data, newItem.children, item.id);
+      }
+    }
+  };
+  /**
+   * 转换方法
+   */
+   const arrayToTree = (data, pid) => {
+      const result = [];
+      getChildren(data, result, pid);
+      return result;
+    };
+  // [
+  //   {
+  //     id: 1,
+  //     name: "部门1",
+  //     pid: 0,
+  //     children: [
+  //       {
+  //         id: 2,
+  //         name: "部门2",
+  //         pid: 1,
+  //         children: [],
+  //       },
+  //       {
+  //         id: 3,
+  //         name: "部门3",
+  //         pid: 1,
+  //         children: [
+  //           {
+  //             id: 4,
+  //             name: "部门4",
+  //             pid: 3,
+  //             children: [
+  //               {
+  //                 id: 5,
+  //                 name: "部门5",
+  //                 pid: 4,
+  //                 children: [],
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // ];
+  
+  
